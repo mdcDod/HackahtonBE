f74f4cab9705ba6199f68671fe3644874c8730e3
Hanna <themdcdod@gmail.com>
2019-07-29 17:40:15 +0300

    add cors

--- DODApp/routes.py
+++ DODApp/routes.py
@@ -3,25 +3,18 @@ from DODApp.APIConnector import contact2Luiz
 from DODApp.APIConnector import contact2KB
 import json
 
+
 @app.route('/')
 @app.route('/dod/<question>')
-
 def dod(question):
     tempDict = {}
     mydata = contact2Luiz(question)
     if mydata[0] == 'None' and mydata[1] == 'None':
-        tempDict.update({"data":"Sorry I can't find answer for your question :("})
-        #return json.loads(tempDict)
+        tempDict.update({"data": "Sorry I can't find answer for your question :("})
     elif mydata[0] == 'GoodGreeting':
-        tempDict.update({"data":"You are so nice!!!!! :)"})
-        #return json.loads("You are so nice!!!!! :)")
+        tempDict.update({"data": "You are so nice!!!!! :)"})
     elif mydata[0] == 'BadGreeting':
-        tempDict.update({"data":"You are not nice!!!!! :("})
-        #return json.loads("You are not nice!!!!! :(")
+        tempDict.update({"data": "You are not nice!!!!! :("})
     else:
-        tempDict.update({"data":contact2KB(mydata[0], mydata[1])})
-        #return json.loads(contact2KB(mydata[0], mydata[1]))
-
-        return tempDict
-
-
+        tempDict.update({"data": contact2KB(mydata[0], mydata[1])})
+    return tempDict
